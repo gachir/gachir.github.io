f7213f370cc9b5f9102f9dc6fc71dc72ebd6bcff
gachir <87061029+gachir@users.noreply.github.com>
2022-01-05 12:03:44 +1000

    Add files via upload

---
+++ Portfolio/time.js
@@ -0,0 +1,12 @@
+
+function display_c(){
+    let refresh=1000; // Refresh rate in milli seconds
+    mytime=setTimeout('display_ct()',refresh)
+    }
+    
+function display_ct() {
+    var x = new Date()
+    document.getElementById('time').innerHTML = x;
+    display_c();
+    }
+    
